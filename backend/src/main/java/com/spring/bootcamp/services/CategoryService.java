b0896ba659c110013436c5e1a978a6e900c3f852
Felipe <felipesand824@gmail.com>
2023-02-23 20:34:27 -0300

    DTO

--- backend/src/main/java/com/spring/bootcamp/services/CategoryService.java
+++ backend/src/main/java/com/spring/bootcamp/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.spring.bootcamp.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.spring.bootcamp.dto.CategoryDTO;
 import com.spring.bootcamp.entities.Category;
 import com.spring.bootcamp.repositories.CategoryRepository;
 
@@ -17,8 +19,10 @@ public class CategoryService {
 	
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 }
