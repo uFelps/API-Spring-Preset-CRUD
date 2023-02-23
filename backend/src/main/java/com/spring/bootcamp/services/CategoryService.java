213fcc9b289bc285065e1c8541663e027546e18a
Felipe <felipesand824@gmail.com>
2023-02-23 20:14:05 -0300

    Database seeding

--- backend/src/main/java/com/spring/bootcamp/services/CategoryService.java
+++ backend/src/main/java/com/spring/bootcamp/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.spring.bootcamp.entities.Category;
 import com.spring.bootcamp.repositories.CategoryRepository;
@@ -15,6 +16,7 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return repository.findAll();
 	}
