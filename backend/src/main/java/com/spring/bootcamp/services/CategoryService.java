82d4cc5b4eca3e29d85bec44edf744a97247db82
Felipe <felipesand824@gmail.com>
2023-02-23 15:21:35 -0300

    H2 database

---
+++ backend/src/main/java/com/spring/bootcamp/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.spring.bootcamp.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.bootcamp.entities.Category;
+import com.spring.bootcamp.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+
+}
