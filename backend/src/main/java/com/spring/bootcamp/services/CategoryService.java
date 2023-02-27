430ffe4e8ca93f15e471117d9b598b18c622ba3d
Felipe <felipesand824@gmail.com>
2023-02-27 19:53:00 -0300

    Pagination

--- backend/src/main/java/com/spring/bootcamp/services/CategoryService.java
+++ backend/src/main/java/com/spring/bootcamp/services/CategoryService.java
@@ -1,11 +1,10 @@
 package com.spring.bootcamp.services;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,10 +23,11 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll() {
-		List<Category> list = repository.findAll();
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+		
+		Page<Category> list = repository.findAll(pageRequest);
 
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		return list.map(x -> new CategoryDTO(x));
 	}
 
 	@Transactional(readOnly = true)
